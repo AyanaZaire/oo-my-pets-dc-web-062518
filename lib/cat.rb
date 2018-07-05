56009ea72d5fe5e52c69fe500cd3ec979311b8f1
github username <github email address>
2018-07-05 13:58:57 -0400

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 class Cat
-  # code goes here
+
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+    #@mood = mood
+  end
+
+#binding.pry
 end
